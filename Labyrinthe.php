a87439605bf114cc7b46eb867afdf7aeabe25044
Alexandre-Hilaire <alexandre.hilaire@le-campus-numerique.fr>
2023-12-13 12:07:18 +0100

    Le terrain s'affiche la partie d'initialise, début de l'implémentation de la fonction check directions

--- Labyrinthe.php
+++ Labyrinthe.php
@@ -1,14 +1,65 @@
 <?php
 $labyrinthe =[
-    [0, 1, 0, 0, 0, 0, 0],
-    [0, 1, 0, 1, 1, 0, 1],
-    [0, 1, 0, 1, 'G', 0, 0],
-    [0, 0, 0, 0, 1, 1, 0],
-    [0, 1, 0, 1, 0, 0, 0],
-    [0, 1, 0, 0, 0, 1, 0]
+    ['.', '□', '.', '.', '.', '.', '.'],
+    ['.', '□', '.', '□', '□', '.', '□'],
+    ['.', '□', '.', '□', '.', '.', '.'],
+    ['.', '.', '.', '.', '□', '□', '.'],
+    ['.', '□', '.', '□', '.', '.', '.'],
+    ['.', '□', '.', '.', '.', '□', '.']
 
 ];
 
-foreach ($labyrinthe as $row){
-    echo " " . implode("\t", $row) . " " . PHP_EOL;
+
+function affiche_terrain($labyrinthe) {
+    sleep(2);
+//    petite boucle pour simuler un rafraichissement de la console
+    for ($i = 0; $i < 50; $i++) {
+        echo PHP_EOL;
+    }
+
+//    Boucle pour afficher le tableau 2D
+    foreach ($labyrinthe as $row) {
+        echo " " . implode("\t", $row) . " " . PHP_EOL;
+    }
 }
+
+//function check($case_joueur, $labyrinthe, $direction){
+//    if
+//    switch ($direction){
+//        case "sud":
+//
+//            break;
+//        case "est":
+//
+//            break;
+//        case "nord":
+//
+//            break;
+//        case "ouest":
+//
+//            break;
+//    }
+//    if (empty($case_cible) && $case_cible !== 1){
+//
+//    }
+//}
+
+$case_depart = 'S';
+$case_arrivee = 'G';
+$coordonees_case_depart = [0, 0];
+$coordonees_case_arrivee = [2, 4];
+
+
+$labyrinthe[$coordonees_case_depart[0]][$coordonees_case_depart[1]] = $case_depart;
+$labyrinthe[$coordonees_case_arrivee[0]][$coordonees_case_arrivee[1]] = $case_arrivee;
+
+affiche_terrain($labyrinthe);
+
+$case_joueur = $coordonees_case_depart;
+//$labyrinthe[$case_joueur[0]][$case_joueur[1]] = 'J';
+
+affiche_terrain($labyrinthe);
+
+//While (($case_joueur) !== $case_arrivee){
+//
+//}
