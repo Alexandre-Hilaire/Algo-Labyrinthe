a6e06fc9e6f43712fbe3bcd06d8c4c71f2b760fc
Alexandre-Hilaire <alexandre.hilaire@le-campus-numerique.fr>
2023-12-15 10:25:28 +0100

    Le labyrinthe est résolu

--- Labyrinthe.php
+++ Labyrinthe.php
@@ -31,24 +31,30 @@ function affiche_terrain($labyrinthe): void
 function check($case_joueur, $labyrinthe) {
     $position_joueur_x = $case_joueur[1];
     $position_joueur_y = $case_joueur[0];
-    $x_max_length = count($labyrinthe[0]) -1;
-    $y_max_length = count($labyrinthe) -1;
+    $x_max_length = count($labyrinthe[0])-1;
+    $y_max_length = count($labyrinthe)-1;
 
-    if ($position_joueur_y < $y_max_length && isset($labyrinthe[$position_joueur_y + 1][$position_joueur_x]) && $labyrinthe[$position_joueur_y + 1][$position_joueur_x] !== "M" && $labyrinthe[$position_joueur_y + 1][$position_joueur_x] === '.') {
+    if (($position_joueur_y < $y_max_length && ($labyrinthe[$position_joueur_y + 1][$position_joueur_x] === '.')) || $labyrinthe[$position_joueur_y + 1][$position_joueur_x] === 'G' ){
         return [$position_joueur_y + 1, $position_joueur_x];
     }
-    else if ($position_joueur_x < $x_max_length && isset($labyrinthe[$position_joueur_y][$position_joueur_x + 1]) && $labyrinthe[$position_joueur_y][$position_joueur_x + 1] !== "M" && $labyrinthe[$position_joueur_y][$position_joueur_x + 1] === '.') {
+    else if (($position_joueur_x < $x_max_length && ($labyrinthe[$position_joueur_y][$position_joueur_x + 1] === '.')) || $labyrinthe[$position_joueur_y][$position_joueur_x + 1] === 'G') {
         return [$position_joueur_y, $position_joueur_x + 1];
     }
-    else if ($position_joueur_y > 0 && $position_joueur_y < $y_max_length && isset($labyrinthe[$position_joueur_y - 1][$position_joueur_x]) && $labyrinthe[$position_joueur_y - 1][$position_joueur_x] !== "M" && $labyrinthe[$position_joueur_y - 1][$position_joueur_x] === '.') {
+    else if ($position_joueur_y > 0  && ($labyrinthe[$position_joueur_y - 1][$position_joueur_x] === '.' || $labyrinthe[$position_joueur_y - 1][$position_joueur_x] === 'G') ) {
         return [$position_joueur_y -1, $position_joueur_x];
     }
-    else if ($position_joueur_x > 0 && $position_joueur_x <= $x_max_length && isset($labyrinthe[$position_joueur_y][$position_joueur_x - 1]) && $labyrinthe[$position_joueur_y][$position_joueur_x - 1] !== "M" && $labyrinthe[$position_joueur_y][$position_joueur_x - 1] === '.') {
+    else if (($position_joueur_x > 0 && ($labyrinthe[$position_joueur_y][$position_joueur_x - 1] === '.')) || $labyrinthe[$position_joueur_y][$position_joueur_x - 1] === 'G') {
         return [$position_joueur_y, $position_joueur_x - 1];
     }
     else {
         return "bloqué";
     }
+}
+function remonter($case_joueur,$labyrinthe){
+    while (check($case_joueur, $labyrinthe) === "bloqué"){
+        $case_joueur = $labyrinthe[$case_joueur[0]][$case_joueur[1]];
+    }
+    return $case_joueur;
 }
 
 $case_depart = 'S';
@@ -61,17 +67,20 @@ $labyrinthe[$coordonees_case_arrivee[0]][$coordonees_case_arrivee[1]] = $case_ar
 
 $case_joueur = $coordonees_case_depart;
 
-sleep(2);
+
 affiche_terrain($labyrinthe);
 While (($case_joueur) !== $coordonees_case_arrivee){
+    sleep(2);
     $case_precedente = $case_joueur;
     if (check($case_joueur, $labyrinthe) === "bloqué") {
         echo "bloqué";
-        die();
+        $case_joueur = remonter($case_joueur, $labyrinthe);
     } else {
         $case_joueur = check($case_joueur, $labyrinthe);
+        $labyrinthe[$case_joueur[0]][$case_joueur[1]] = [$case_precedente[0], $case_precedente[1]];
     }
-    $labyrinthe[$case_joueur[0]][$case_joueur[1]] = [$case_precedente[0], $case_precedente[1]];
+
 
     affiche_terrain($labyrinthe);
 }
+echo "La sortie à été trouvée !";
